0a6813289c7e2f2592023f7a14c0a5fae7e8725d
Muhammad Baghiz A <ahmadvallden86@gmail.com>
2026-08-08 13:49:58 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,16 @@
+/**
+ * config.js
+ * Konfigurasi frontend Symphony Course Manager.
+ *
+ * PENTING: Ganti nilai API_URL di bawah dengan URL Web App Google Apps Script Anda
+ * SETELAH melakukan deployment (Deploy > New deployment > Web app).
+ * Contoh: "https://script.google.com/macros/s/AKfycbx....../exec"
+ *
+ * Jangan pernah memasukkan URL asli ke dalam repository publik jika bersifat rahasia.
+ */
+const CONFIG = {
+  API_URL:
+    "https://script.google.com/macros/s/AKfycbyTCEAx-fChs6cULzljA3jyWXCoXzqJf7FrqgwCs9M5edT4dirUTXL76Y8FduWO-FsM/exec",
+  APP_NAME: "Symphony Course Manager",
+  CURRENCY: "IDR",
+};
